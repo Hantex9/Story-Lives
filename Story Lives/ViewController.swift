661b01ca93d43b183333ec39910ae47532a4a447
Alessandro Izzo <alessandroizzo@10-20-50-173.0200.fed.it.iosda.org>
2017-12-11 15:59:27 +0100

    Initial Commit

---
+++ Story Lives/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Story Lives
+//
+//  Created by Alessandro Izzo on 11/12/17.
+//  Copyright © 2017 Alessandro Izzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
